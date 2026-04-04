b9dedf9f7622b2b06eddc60614777dd3f9db4ad2
James Sexton <0sextj12@solent.ac.uk>
2026-04-04 21:08:04 +0100

    Created MainViewModel.kt with variables for latitude, longitude and zoom. Also added the maplibre library to libs.versions.toml and build.gradle.kts.

---
+++ app/src/main/java/com/example/pointsofinterestprojectq102550212/MainViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.pointsofinterestprojectq102550212
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import org.maplibre.android.geometry.LatLng
+
+class MainViewModel: ViewModel() {
+    var latLng = LatLng(50.9079, -1.4015)
+        set(newValue) {
+            field = newValue
+            latLngLiveData.value = newValue
+        }
+    var latLngLiveData = MutableLiveData<LatLng>()
+
+    var zoom: Double = 14.0
+        set(newValue) {
+            field = newValue
+            zoomLiveData.value = newValue
+        }
+    var zoomLiveData = MutableLiveData<Double>()
+}
